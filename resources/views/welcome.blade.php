b2c6ed788954b9a69219c3228cbced592aa485fc
jose vladimir cruz <cel69847163@gmail.com>
2020-10-27 21:45:15 -0400

    Primer cambio

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Esta es la Rama de Master
+                    Esta es la Rama de josecr
                 </div>
 
                 <div class="links">
